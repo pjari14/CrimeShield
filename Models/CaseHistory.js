b7e7cb894236c057f371f3e4d0111c8c5247f390
pjari14 <prishajariwala14@gmail.com>
2024-03-07 18:31:14 +0530

    Update CaseHistory.js

--- Models/CaseHistory.js
+++ Models/CaseHistory.js
@@ -1,27 +1,25 @@
-
 const mongoose = require("mongoose");
 
 const caseSchema = new mongoose.Schema(
-    {
-        CrimeID:{
-            type: mongoose.Types.ObjectId,
-            ref: "crimes",
-            required: true,
-        },
-        Description:{
-            type: String,
-            default : null,
-            required:false,
-        },
-        Date:{
-            type: Date,
-            default: Date.now,
-        },
-        
+  {
+    CrimeID: {
+      type: mongoose.Types.ObjectId,
+      ref: "crimes",
+      required: true,
     },
-    {
-        timestamps: true,
-      }
+    Description: {
+      type: String,
+      default: null,
+      required: false,
+    },
+    Date: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 const casehistory = mongoose.model("casehistory", caseSchema);
