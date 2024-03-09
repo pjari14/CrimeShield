2af72fbdb85ac1b6173786aa46d8e093d5f1232f
Prachi8911 <160835198+Prachi8911@users.noreply.github.com>
2024-03-09 08:42:32 +0530

    Link valu

--- src/Pages/crimeprevention.js
+++ src/Pages/crimeprevention.js
@@ -1,9 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
 const Prevention = () => {
   return (
     <>
-      <div class="col-md-4"></div>
+    <div class="crime">
+    <div class="container mt-1 pt-1">
+    
+      <div class="col-md-12">
+      
+        <h3 class="p-3 text-center mb-2 fs-2 font-weight-bold"
+          style={{color: "#D64550", fontWeight: "bold", fontFamily: "Merriweather" }}
+        >
+          CRIME PREVENTION
+          </h3>
+          </div>
+          </div>
+          </div>
     </>
   );
 };
