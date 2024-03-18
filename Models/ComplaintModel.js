378eacf538a4d3db5cf84390f9dee3b3d3bd3853
pjari14 <prishajariwala14@gmail.com>
2024-03-18 13:47:01 +0530

    commit

--- Models/ComplaintModel.js
+++ Models/ComplaintModel.js
@@ -1,37 +1,36 @@
-const mongoose = require ("mongoose");
+const mongoose = require("mongoose");
 
-const ComplaintSchema  =new mongoose.Schema({
-
-    fname: {
-        type:String,
-        required:[true,"First name is required"]
-    },
-    lname: {
-        type:String,
-        required: [true,"Last name is required"]
-    },
-    email: {
-        type:String,
-        required:[true,'Email is required']
-    },
-    contactno:{
-        type:String,
-        required:[true,'Contact number is required']
-    },
-    address: {
-        type:String,
-        required:[true,"Address is required"]
-    },
-    idproof:{
-        type:String
-    },
-    date: {
-        type: Date,
-        required:[true,"Date is required"],
-        default: Date.now
-    }
+const ComplaintSchema = new mongoose.Schema({
+  fname: {
+    type: String,
+    required: [true, "First name is required"],
+  },
+  lname: {
+    type: String,
+    required: [true, "Last name is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+  },
+  contactno: {
+    type: String,
+    required: [true, "Contact number is required"],
+  },
+  address: {
+    type: String,
+    required: [true, "Address is required"],
+  },
+  idproof: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    required: [true, "Date is required"],
+    default: Date.now,
+  },
 });
 
-const Complaint = mongoose.model("complaints",ComplaintSchema); 
+const Complaint = mongoose.model("complaints", ComplaintSchema);
 
 module.exports = Complaint;
--- Models/Incident.js
+++ Models/Incident.js
@@ -1,60 +1,57 @@
 const mongoose = require("mongoose");
-const {ComplaintSchema} = require("./ComplaintModel");
+const { ComplaintSchema } = require("./ComplaintModel");
 
-const incidentSchema = new mongoose.Schema(
-    {
-        complaintID: {
-            // type: ComplaintSchema,
-            type: mongoose.Types.ObjectId,
-            // ref: "complaints",
-            required: true,
-        },
-        category:{
-            type:String,
-            required:true,
-            enum: ["Cyber Crime","Domestic Violence","Abuse","Theft","Robbery"]
-        },
-        dateofcmp: {
-            type : Date,
-            default:Date.now,
-        },
-        
-        state: {
-            type: String,
-            required: true,
-        },
-        city: {
-            type: String,
-            required: true,
-        },
-        policestation: {
-            type:String,
-            required: true,
-        },
-        datentime: {
-            type: Date
-        },
-        reasonofdelay: {
-            type:String
-        },
-        location: {
-            type: String
-        },
-        evidence: {
-            type: String
-        },
-        nameofsus: {
-            type: String
-        },
-        additionalinfo: {
-            type: String,
-            required: false,
-            default: null
-        },
-        
-    }
-);
+const incidentSchema = new mongoose.Schema({
+  complaintID: {
+    // type: ComplaintSchema,
+    type: mongoose.Types.ObjectId,
+    // ref: "complaints",
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+    enum: ["Cyber Crime", "Domestic Violence", "Abuse", "Theft", "Robbery"],
+  },
+  dateofcmp: {
+    type: Date,
+    default: Date.now,
+  },
 
-const incident = new mongoose.model("incident",incidentSchema);
+  state: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  policestation: {
+    type: String,
+    required: true,
+  },
+  datentime: {
+    type: Date,
+  },
+  reasonofdelay: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  evidence: {
+    type: String,
+  },
+  nameofsus: {
+    type: String,
+  },
+  additionalinfo: {
+    type: String,
+    required: false,
+    default: null,
+  },
+});
+
+const incident = new mongoose.model("incident", incidentSchema);
 
 module.exports = incident;
